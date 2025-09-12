82837ba8f5855cf64631b2ec1e8c602732307aec
kintoyyy <56209108+Kintoyyy@users.noreply.github.com>
2025-09-12 22:06:47 +0800

    intialize basic pages

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
   const [count, setCount] = useState(0);
@@ -15,6 +16,13 @@ export default function Home() {
         Increment Count: {count}
       </button>
 
+      <button className="mt-4 px-5 py-2 bg-blue-500 text-white font-medium rounded-lg shadow hover:bg-blue-600 transition">
+        <Link href="/login">login page</Link>
+      </button>
+
+      <button className="mt-4 px-5 py-2 bg-purple-500 text-white font-medium rounded-lg shadow hover:bg-purple-600 transition">
+        <Link href="/register">register page</Link>
+      </button>
     </div>
   );
 }
---
+++ pages/register.js
@@ -0,0 +1,7 @@
+export default function Register() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen text-center bg-background">
+            <h1 className="text-5xl font-bold text-primary">Register Page!</h1>
+        </div>
+    );
+}
