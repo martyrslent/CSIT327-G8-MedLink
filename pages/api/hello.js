546fa1c4be06244f7d51255ef1fc1e396146ce16
kintoyyy <56209108+Kintoyyy@users.noreply.github.com>
2025-09-24 21:34:23 +0800

    remove js

--- pages/api/hello.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-// Example: Access via
-// http://localhost:3000/api/hello
-
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
-}
--- pages/login.js
+++
@@ -1,7 +0,0 @@
-export default function Login() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen text-center bg-background">
-            <h1 className="text-5xl font-bold text-primary">Login Page!</h1>
-        </div>
-    );
-}
