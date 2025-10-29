3128f8b5c60093c094a19cff6104606a86dc18c9
Ed Lawrenz C. Portes <martyrslent@users.noreply.github.com>
2025-10-29 20:01:42 +0800

    Save local changes before rebase

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn medlink.wsgi
--- medlink/settings.py
+++ medlink/settings.py
@@ -25,17 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8)4%rtw&mvalj*80oh1u-#k)e5j80ro6#d(i%^&cs7z7c&yb#c'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = [
-    "10.15.0.165",
-    "localhost",
-    "127.0.0.1",
-    "medlink.kintoyyy.com"
-]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "medlink.kintoyyy.com", ".onrender.com"]
 
 
 # Application definition
@@ -52,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -83,11 +79,12 @@ WSGI_APPLICATION = 'medlink.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config("DATABASE_URL")
+    )
 }
 
 
@@ -135,3 +132,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
